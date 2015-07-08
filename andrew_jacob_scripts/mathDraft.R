46f3ab58cb1b4c3d4691dfbfdca8cd5246f503ba
Andrew Giles <agiles231@gmail.com>
2015-07-08 11:10:55 -0400

    Integrated the code that performs the simulation calculation and plotting into the shiny app.

--- andrew_jacob_scripts/mathDraft.R
+++ andrew_jacob_scripts/mathDraft.R
@@ -2,31 +2,33 @@
 m <- 1000   # carrying capacity
 b  <- 0.05  # birth rate
 d  <- 0.025 # death rate
-n0 <- 2
-nt <- n0   # initial value, garbage value to start
-nt_1 <- nt # value from last time step
+n_0 <- 2    # Initial Poplulation
+n_t <- n_0   # initial value, garbage value to start
+n_t_1 <- n_t # value from last time step
 totalTime <- 550
 t <- 0
-simulated_nt <- n0 # initial value, garbage value to start
+nt <- n_0 # initial value, garbage value to start
 
 
-bt <- 0 # Initial values to make these objects persistant
-dt <- 0 # Initial values to make these objects persistant
-st <- 0 # Initial values to make these objects persistant
-lt <- 0 # Initial values to make these objects persistant
-Lt <- 0 # Initial values to make these objects persistant
-littersizes <- list(rep(-1, totalTime + 1)) # Initial values to make these objects persistant
+bt <- 0 # Initial values to make these objects persistan_t
+dt <- 0 # Initial values to make these objects persistan_t
+st <- 0 # Initial values to make these objects persistan_t
+lt <- 0 # Initial values to make these objects persistan_t
+Lt <- 0 # Initial values to make these objects persistan_t
+littersizes <- list(rep(-1, totalTime + 1)) # Initial values to make these objects persistan_t
+dead <- list(rep(-1, totalTime + 1)) # Initial values to make these objects persistan_t
+living <- list(rep(-1, totalTime + 1)) # Initial values to make these objects
 
 
 daframe <- data.frame(time = rep(0, totalTime + 1), numLitters = rep(0, totalTime + 1)
-                      , population = rep(n0, totalTime + 1)
-                      , birthrate = rep(n0, totalTime + 1), born = rep(0, totalTime + 1)
+                      , population = rep(n_0, totalTime + 1)
+                      , birthrate = rep(n_0, totalTime + 1), born = rep(0, totalTime + 1)
                       , deathrate = rep(0, totalTime + 1), dead = rep(0, totalTime + 1)
-                      , theoretical = rep(n0, totalTime + 1))
+                      , theoretical = rep(n_0, totalTime + 1))
 dfrow <- numeric(length = 8)
 dfrow[1] <- t
 dfrow[2] <- Lt
-dfrow[3] <- simulated_nt
+dfrow[3] <- nt
 dfrow[4] <- bt
 dfrow[5] <- 0
 dfrow[6] <- 0
@@ -35,35 +37,50 @@ dfrow[8] <- 0
 
 update <- function() {
   t <<- t + 1
-  bstar <-  b + (d - b)/2*(simulated_nt/m) # birth rate at time t
-  dstar <-  d + (b - d)/2*(simulated_nt/m) # death rate at time t
-  bt <<- max(0, bstar)
-  dt <<- dstar + max(0, -bstar)
-  st <<- sqrt(bt) # averate size of litters
-  lt <<- sqrt(bt) # rate of litters
-  Lt <<- rpois(n = 1, lambda = lt*simulated_nt) # number of litters based on litter rate
+  bstar <-  b + (d - b)/2*(nt/m) # Important factor in birth rate at time t
+  dstar <-  d + (b - d)/2*(nt/m) # Important factor in death rate at time t
+  bt <<- max(0, bstar)          # birth rate at time t
+  dt <<- dstar + max(0, -bstar) # death rate at time t
+  st <<- 8*sqrt(bt)/sqrt(b) # averate size of litters
+  lt <<- sqrt(bt)*sqrt(b)/8 # rate of litters
+  Lt <<- rpois(n = 1, lambda = lt*nt) # number of litters based on litter rate
   if (is.null(littersizes)) {
-    littersizes <<- list(rpois(Lt, lambda = st)) # produce sizes of each litter
   }
   else {
     littersizes[[t]] <<- rpois(Lt, lambda = st) # produce sizes of each litter
   }
-  dead <- rpois(1, lambda = dt*simulated_nt) # produce dead rabbits
+  if (is.null(dead)) {
+  }
+  else {
+    dead[[t]] <<- rpois(1, lambda = dt*nt) # produce dead rabbits
+  }
+
+  # keep track of the living rabbits and when they were born
+  birthsThisGeneration <- sum(littersizes[[t]])
+  if (birthsThisGeneration >= 1) {
+    living[[t]] <<- 1:birthsThisGeneration
+  }
+  else {
+    living[[t]] <<- NULL
+  }
+  for (i in 1:dead[[t]]) {
+  }
+
   if (length(littersizes[[t]])) {
     # population sizes = previous population + sum of all litters - dead rabbits
-    simulated_nt <<- simulated_nt + sum(littersizes[[t]]) - dead
+    nt <<- nt + sum(littersizes[[t]]) - sum(dead[[t]])
   }
 
   dfrow[1] <- t
   dfrow[2] <- Lt
-  dfrow[3] <- simulated_nt
+  dfrow[3] <- nt
   dfrow[4] <- bt
   dfrow[5] <- sum(littersizes[[t]])
   dfrow[6] <- dt
-  dfrow[7] <- dead
+  dfrow[7] <- sum(dead[[t]])
   # below line is jacob's broken equation
-  # dfrow[8] <- ((m*n0)/(m-n0)*exp((b-d)/(m^2)*t))/(1+(n0)/(m-n0)*exp((b-d)/(m^2)*t))
-  dfrow[8] <- m / (1 + ((m-n0)/n0)*exp(-1*(b-d)*t))
+  # dfrow[8] <- ((m*n_0)/(m-n_0)*exp((b-d)/(m^2)*t))/(1+(n_0)/(m-n_0)*exp((b-d)/(m^2)*t))
+  dfrow[8] <- m / (1 + ((m-n_0)/n_0)*exp(-1*(b-d)*t))
   daframe[t+1,] <<- dfrow
   #####
 }
@@ -72,6 +89,7 @@ update <- function() {
 for (i in 1:totalTime) {
   update()
 }
+
 png(paste(as.character(as.numeric(Sys.time())),"modelplot.png", sep=""), width = 480, height = 480)
 plot(x=daframe$time, daframe$population, ylab = "Population (n)"
      , xlab = "Time (Insert Units)")
--- logistic/server.R
+++ logistic/server.R
@@ -1,66 +1,137 @@
 library(shiny)
 
-n0<-1
-max.time<-500
-max.Rmax<-1
-max.k<-1000
+
+popGraph <- function(m, b, d, n_0, totalTime) {
+  totalTime <- 550
+  t <- 0
+  n_t <- n_0 # initial value, garbage value to start
+
+
+  bt <- 0 # Initial values to make these objects persistant
+  dt <- 0 # Initial values to make these objects persistant
+  st <- 0 # Initial values to make these objects persistant
+  lt <- 0 # Initial values to make these objects persistant
+  Lt <- 0 # Initial values to make these objects persistant
+  littersizes <- list(rep(-1, totalTime + 1)) # Initial values to make these objects persistan_t
+  dead <- list(rep(-1, totalTime + 1)) # Initial values to make these objects persistant
+  living <- list(rep(-1, totalTime + 1)) # Initial values to make these objects
+
+
+  daframe <- data.frame(time = rep(0, totalTime + 1), numLitters = rep(0, totalTime + 1)
+                        , population = rep(n_0, totalTime + 1)
+                        , birthrate = rep(n_0, totalTime + 1), born = rep(0, totalTime + 1)
+                        , deathrate = rep(0, totalTime + 1), dead = rep(0, totalTime + 1)
+                        , theoretical = rep(n_0, totalTime + 1))
+  dfrow <- numeric(length = 8)
+  dfrow[1] <- t
+  dfrow[2] <- Lt
+  dfrow[3] <- n_t
+  dfrow[4] <- bt
+  dfrow[5] <- 0
+  dfrow[6] <- 0
+  dfrow[7] <- 0
+  dfrow[8] <- 0
+
+  update <- function() {
+    t <<- t + 1
+    bstar <-  b + (d - b)/2*(n_t/m) # Important factor in birth rate at time t
+    dstar <-  d + (b - d)/2*(n_t/m) # Important factor in death rate at time t
+    bt <<- max(0, bstar)          # birth rate at time t
+    dt <<- dstar + max(0, -bstar) # death rate at time t
+    st <<- 8*sqrt(bt)/sqrt(b) # averate size of litters
+    lt <<- sqrt(bt)*sqrt(b)/8 # rate of litters
+    Lt <<- rpois(n = 1, lambda = lt*n_t) # number of litters based on litter rate
+    if (is.null(littersizes)) {} # Do nothing
+    else {
+      littersizes[[t]] <<- rpois(Lt, lambda = st) # produce sizes of each litter
+    }
+    if (is.null(dead)) {} # Do nothing
+    else {
+      dead[[t]] <<- rpois(1, lambda = dt*n_t) # produce dead rabbits
+    }
+
+    # keep track of the living rabbits and when they were born
+    birthsThisGeneration <- sum(littersizes[[t]])
+    if (birthsThisGeneration >= 1) {
+      living[[t]] <<- 1:birthsThisGeneration
+    }
+    else {
+      living[[t]] <<- NULL
+    }
+    for (i in 1:dead[[t]]) {
+    }
+
+    if (length(littersizes[[t]])) {
+      # population sizes = previous population + sum of all litters - dead rabbits
+      n_t <<- n_t + sum(littersizes[[t]]) - sum(dead[[t]])
+    }
+
+    dfrow[1] <- t
+    dfrow[2] <- Lt
+    dfrow[3] <- n_t
+    dfrow[4] <- bt
+    dfrow[5] <- sum(littersizes[[t]])
+    dfrow[6] <- dt
+    dfrow[7] <- sum(dead[[t]])
+    # below line is jacob's broken equation
+    # dfrow[8] <- ((m*n_0)/(m-n_0)*exp((b-d)/(m^2)*t))/(1+(n_0)/(m-n_0)*exp((b-d)/(m^2)*t))
+    dfrow[8] <- m / (1 + ((m-n_0)/n_0)*exp(-1*(b-d)*t))
+    daframe[t+1,] <<- dfrow
+    #####
+  }
+
+
+  for (i in 1:totalTime) {
+    update()
+  }
+
+  plot(x=daframe$time, daframe$population, ylab = "Population (n)"
+       , xlab = "Time (Insert Units)")
+
+  lines(x=daframe$time, daframe$theoretical, col = "red")
+
+}
 
 function(input, output) {
-  
-  rv <- reactiveValues(
-    t = NULL,
-    ne = vector(),
-    nl = vector(),
-    Re = vector(),
-    re = vector(),
-    rl = vector(),
-    Rl = vector()
-  )
-  
-  observe({
-    rv$t <- seq(0,input$time,1)
-    # exponential growth
-    for (i in 1:length(rv$t)) {
-      if(i==1) {rv$ne[i]<-n0; rv$re[i]<-input$Rmax; rv$Re[i]<-0; next}
-      rv$ne[i]<-rv$ne[i-1]+rv$ne[i-1]*input$Rmax
-      rv$re[i]<-input$Rmax
-      rv$Re[i]<-rv$ne[i]-rv$ne[i-1]
-    }
-    # logistic growth
-    for (i in 1:length(rv$t)) {
-      if(i==1) {rv$nl[i]<-n0; rv$rl[i]<-input$Rmax; rv$Rl[i]<-0; next}
-      rv$nl[i]<-rv$nl[i-1]+rv$nl[i-1]*input$Rmax*((input$k-rv$nl[i-1])/input$k)
-      rv$rl[i]<-input$Rmax*((input$k-rv$nl[i])/input$k)
-      rv$Rl[i]<-rv$nl[i]-rv$nl[i-1]
-    }
-  })
-  
+
+#  rv <- reactiveValues(
+#    t = NULL,
+#    ne = vector(),
+#    nl = vector(),
+#    Re = vector(),
+#    re = vector(),
+#    rl = vector(),
+#    Rl = vector()
+#  )
+
+
   output$pop <- renderPlot({
-    plot(rv$t,rv$ne, ylim=c(0,max.k*1.1), type="l", lwd=2, col="red",
-         main="Population Size over time", cex.main=0.8,
-         xlab="time", ylab="pop. size (n)", cex.lab=0.8)
-    points(rv$t,rv$nl, type="l", lwd=2, col="black")
-    abline(h=input$k, lty=3, lwd=3, col="black")
+    input$goButton
+    popGraph(  isolate(input$m)
+             , isolate(input$b)
+             , isolate(input$d)
+             , isolate(input$n_0)
+             , isolate(input$totalTime))
   })
-  
-  output$growth <- renderPlot({
-    if(input$growthRate==1) {
-      plot(rv$ne,rv$Re, type="l", xlim=c(0,input$k*1.05), ylim=c(0,max(rv$Rl)*2), 
-           lwd=2, col="red",
-           main="Population Growth Rate (R) v. Population Size (n)", 
-           xlab="number of individuals (n)", ylab="new individuals/ time (R)", 
-           cex.main=0.8, cex.lab=0.8, cex.axis=0.8)
-      points(rv$nl,rv$Rl, type="l", lwd=2, col="black")
-    }
-    
-    if(input$growthRate==2) {
-      plot(rv$ne,rv$re, type="l", xlim=c(0,max.k*1.05), ylim=c(0,max.Rmax*1.05) , 
-           lwd=2, col="red",
-           main="Per Capita Growth Rate (r) v. Population Size (n)", 
-           xlab="number of individuals (n)", ylab="new individuals/ time/ n (r)", 
-           cex.main=0.8, cex.axis=0.8, cex.lab=0.8)
-      points(rv$nl,rv$rl, type="l", lwd=2, col="black")
-    }
-  })
-  
+
+#  output$growth <- renderPlot({
+#    if(input$growthRate==1) {
+#      plot(rv$ne,rv$Re, type="l", xlim=c(0,input$k*1.05), ylim=c(0,max(rv$Rl)*2),
+#           lwd=2, col="red",
+#           main="Population Growth Rate (R) v. Population Size (n)",
+#           xlab="number of individuals (n)", ylab="new individuals/ time (R)",
+#           cex.main=0.8, cex.lab=0.8, cex.axis=0.8)
+#      points(rv$nl,rv$Rl, type="l", lwd=2, col="black")
+#    }
+
+#    if(input$growthRate==2) {
+#      plot(rv$ne,rv$re, type="l", xlim=c(0,max.k*1.05), ylim=c(0,max.Rmax*1.05) ,
+#           lwd=2, col="red",
+#           main="Per Capita Growth Rate (r) v. Population Size (n)",
+#           xlab="number of individuals (n)", ylab="new individuals/ time/ n (r)",
+#           cex.main=0.8, cex.axis=0.8, cex.lab=0.8)
+#      points(rv$nl,rv$rl, type="l", lwd=2, col="black")
+#    }
+#  })
+
 }
