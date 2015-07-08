a292a386ecf9c3c8ee1974f621509febb3fa9c31
Jacob Townson <obewanjacobi@aim.com>
2015-07-08 11:09:29 -0400

    New ui for app.

--- logistic/ui.R
+++ logistic/ui.R
@@ -1,16 +1,25 @@
-max.time<-500
-max.Rmax<-1
-max.k<-1000
+max.time<-1000
+max.b <- 1
+min.d <- 1
+max.m <-1000
+max.n_0 <- 20
 
 fluidPage(
   titlePanel("Logistic Growth"),
   sidebarPanel(
-    sliderInput(inputId = "time", label = "Extent of Time", min = 0, 
-                max = max.time, value = max.time*0.2),
-    sliderInput(inputId = "Rmax", label = "Rmax",
-                min = 0, max = max.Rmax, value = max.Rmax*0.1),
-    sliderInput(inputId = "k", label = "Carrying Capacity",
-                min = 10, max = max.k, value = max.k*0.8)
+    sliderInput(inputId = "totalTime", label = "Extent of Time", 
+                min = max.time*.1, max = max.time, value = max.time*0.2),
+    sliderInput(inputId = "n_0", label = "Initial Population", 
+                min = 2, max = max.n_0, value = 5, step = 1),
+    sliderInput(inputId = "b", label = "Max Birth Rate",
+                min = 0, max = max.b, value = .05),
+    sliderInput(inputId = "d", label = "Min Death Rate",
+                min = 0, max = min.d, value = .02),
+    sliderInput(inputId = "m", label = "Carrying Capacity",
+                min = 10, max = max.m, value = max.m*0.8),
+    actionButton("goButton", "Simulate"),
+    helpText("Click the button to display the",
+              "graph with the inputs you selected.")
   ),
   mainPanel(
     tabsetPanel(
@@ -19,11 +28,13 @@ fluidPage(
         plotOutput("pop")
       ),
       tabPanel(
-        title = "Growth Rate",
-        plotOutput("growth"),
-        radioButtons(inputId = "growthRate", label = "Type of Growth Rate to Show",
-                    choices = c("Population Growth Rate" =1, "Per Capita Growth Rate" =2),
-                    selected = 1)
+        title = "Growth Rate"
+      ),
+      tabPanel(
+        title = "Field"
+      ),
+      tabPanel(
+        title = "Graveyard"
       ),
       id = "tabset"
     )
