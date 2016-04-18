597f84fdcc5ac21332e3104fc01d448c7eff632c
Jacob Townson <obewanjacobi@aim.com>
2016-04-17 21:59:03 -0400

    post scores added for Dr. Griffith's class

--- thesis/pre and post test scores.R
+++ thesis/pre and post test scores.R
@@ -3,10 +3,10 @@
 kstudents <- c(1:14)
 gstudents <- c(1:12)
 
-gprescore <- c(6/11, 5/11, 6/11, 7/11, 7/11, 4/11, 6/11, 5/11, 6/11, 3/11, 5/11, 5/11)
+gprescore <- c(6/11, 4/11, 6/11, 7/11, 7/11, 4/11, 6/11, 4/11, 6/11, 3/11, 5/11, 5/11)
 kprescore <- c(6/11, 4/11, 6/11, 7/11, 3/11, 6/11, 3/11, 8/11, 5/11, 4/11, 6/11, 6/11, 6/11, 6/11)
 
-gpostscore <- c(rep(NA, 12))
+gpostscore <- c(8/11, 5/11, 5/11, 5/11, 9/11, 8/11, 8/11, 3/11, NA, 6/11, 8/11, 7/11)
 kpostscore <- c(rep(NA, 14))
 
 tgriff <- data.frame(gstudents, gprescore, gpostscore)
