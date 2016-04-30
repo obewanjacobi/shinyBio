b06701ac767ebe0a6fb5b270c8c5294e52250044
Jacob Townson <obewanjacobi@aim.com>
2016-04-30 13:32:49 -0400

    Updated code to do the t-test.

--- thesis/pre and post test scores.R
+++ thesis/pre and post test scores.R
@@ -12,3 +12,21 @@ kpostscore <- c(5/11, NA, 6/11, 6/11, 4/11, 7/11, 1/11, 9/11, 4/11, 5/11, 9/11, 
 tgriff <- data.frame(gstudents, gprescore, gpostscore)
 
 kopp <- data.frame(kstudents, kprescore, kpostscore)
+
+gp <- gprescore/100*11
+kp <- kprescore/100*11
+ga <- gpostscore/100*11
+ka <- kpostscore/100*11
+inst <- c(rep("griffith", 12), rep("kopp",14))
+prepost <- data.frame(instructor = inst, pre = c(gp,kp), post = c(ga, ka))
+prepost$diff <- prepost$post - prepost$pre
+
+t.test(diff ~ instructor, data = prepost)
+
+library(lattice)
+lattice::histogram( ~ diff | instructor,
+                    data = prepost,
+                    layout = c(1,2), 
+                    breaks = seq(from = -2.5, to = 4.5, by = 1),
+                    type = "count",
+                    xlab = "score difference (post - pre)")
